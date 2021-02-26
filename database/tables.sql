dc8a2d561bdc7d47ab04ecbd0d5316949b3e1f30
Sean Guo <slg210@rutgers.edu>
2021-02-25 21:13:54 -0500

    tables.sql edited online with Bitbucket
    Create table statements are updated. They have been tested to work for Sean.

--- database/tables.sql
+++ database/tables.sql
@@ -1,8 +1,35 @@
 USE `cht`;
 
 CREATE TABLE `users` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(150) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  PRIMARY KEY (`id`)
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `county` varchar(50) NOT NULL,
+  PRIMARY KEY (`username`)
+);
+
+CREATE TABLE `travels` (
+  `username` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `county` varchar(50) NOT NULL,
+  KEY `usernameTravels_idx` (`username`),
+  CONSTRAINT `usernameTravels` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+);
+
+CREATE TABLE `tests` (
+  `username` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `test` varchar(50) NOT NULL,
+  `result` varchar(50) NOT NULL,
+  KEY `usernameTests_idx` (`username`),
+  CONSTRAINT `usernameTests` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+);
+
+CREATE TABLE `symptoms` (
+  `username` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `symptoms` varchar(200) NOT NULL,
+  KEY `usernameSymptoms_idx` (`username`),
+  CONSTRAINT `usernameSymptoms` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 );
