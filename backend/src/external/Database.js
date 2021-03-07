e63abea7f50e646d30d3ef73f6c8b3c49b5ccfe1
syall <s.yuan.all@gmail.com>
2021-03-07 04:02:15 -0500

    CHT-37 Create API Route to Update User Information
    - Added the UserRouter in CovidHealthTracker.js
    - Added a patchUser query in Database.js to update existing users
    - Implemented UserRouter.js with two routes:
      - GET /user/profile/:username to get info about a user
      - PATCH /user/profile/:username to update a user

--- backend/src/external/Database.js
+++ backend/src/external/Database.js
@@ -61,6 +61,78 @@ class Database {
         });
     }
 
+    patchUser(user, args) {
+        // Build SET query based on differences in current row and arguments
+        const buildArgs = (prev, next) => {
+            try {
+                const query = [];
+                for (const key of Object.keys(prev)) {
+                    // If empty input or unchangeable field
+                    if (prev[key] === next[key] ||
+                        next[key] === '' ||
+                        next[key] === null ||
+                        key === 'admin' ||
+                        key === 'id') {
+                        continue;
+                    }
+                    // Encrypt password
+                    if (key === 'password') {
+                        const encrypted = Encryption.encrypt(next[key], this.SALT);
+                        if (prev[key] !== encrypted) {
+                            query.push(`${key} = '${encrypted}'`);
+                        }
+                        // Convert age
+                    } else if (key === 'age') {
+                        query.push(`${key} = ${Number(next[key])}`);
+                        // Convert atRisk
+                    } else if (key === 'atRisk') {
+                        if ((prev[key] === 1) !== next[key]) {
+                            query.push(`${key} = ${Boolean(next[key])}`);
+                        }
+                        // Otherwise, build
+                    } else {
+                        query.push(`${key} = '${next[key]}'`);
+                    }
+                }
+                return query.join(', ');
+            } catch (error) {
+                return error;
+            }
+        };
+        return new Promise((resolve, reject) => {
+            const connection = mysql.createConnection({
+                host: this.DB_HOST,
+                user: this.DB_USER,
+                password: this.DB_PASSWORD
+            });
+            connection.connect((err) => {
+                if (err) {
+                    connection.end();
+                    return reject(err);
+                }
+                connection.query('USE cht');
+                const builtArgs = buildArgs(user, args);
+                if (builtArgs === '') {
+                    return resolve('no op');
+                } else if (builtArgs instanceof Error) {
+                    return reject('Error updating profile');
+                }
+                const updateQuery = `
+                    UPDATE users
+                    SET ${builtArgs}
+                    WHERE username = '${user.username}';
+                `;
+                connection.query(updateQuery, (err) => {
+                    connection.end();
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve('success');
+                });
+            });
+        });
+    }
+
     matchUser(username, password) {
         return new Promise((resolve, reject) => {
             const connection = mysql.createConnection({
