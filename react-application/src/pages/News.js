97d3a80cb60dcba9a5cc7674106ed7373345c8c0
Gregorio <grf27@rutgers.edu>
2021-03-24 16:33:57 -0400

    properly displays api data in news page

--- react-application/src/pages/News.js
+++ react-application/src/pages/News.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Image, Col } from "react-bootstrap";
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel } from '@material-ui/core';
+import { Toolbar, Typography, Paper, Checkbox, IconButton, Tooltip, FormControlLabel, Switch } from '@material-ui/core';
 
 const useFetch = () => {
     const [news, setNews] = useState(null);
@@ -21,7 +25,7 @@ const useFetch = () => {
             "method": "GET",
         });
         const data = await response.json();
-        setNews(data);
+        setNews(data.articles);
         setLoading(false);
     }, []);
 
@@ -30,13 +34,41 @@ const useFetch = () => {
 
 const News = () => {
     const { news, isLoading } = useFetch();
+    /** News Fields
+     *  .author -> Article Author
+     *  .content -> Full article body
+     *  .description -> Short article summary
+     *  .source.name -> Source
+     *  .title -> Title
+     *  .url -> Article url
+     *  .urlToImage -> Article Image
+     */
     return (
         <>
             {isLoading ? <div>Loading...</div> :
-                <Jumbotron fluid style={{ background: "#20475A" }}>
-                    <p>News Page!</p>
-                    <p>{news.articles[0].author}</p>
-                </Jumbotron>
+                <Paper>
+                    <Jumbotron fluid style={{ background: "#20475A" }}>
+                        <p>News Page!</p>
+                        <p>{news[0].author}</p>
+                    </Jumbotron>
+                    <TableContainer>
+                        <Table>
+                            <TableBody>
+                                {news.map((article) => {
+                                    return (
+                                        <TableRow>
+                                            <TableCell><Image src={article.urlToImage} thumbnail /></TableCell>
+                                            <TableCell>{article.title}</TableCell>
+                                            <TableCell>{article.author}</TableCell>
+                                            <TableCell>{article.description}</TableCell>
+                                        </TableRow>
+                                    )
+
+                                })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
             }
         </>
     );
